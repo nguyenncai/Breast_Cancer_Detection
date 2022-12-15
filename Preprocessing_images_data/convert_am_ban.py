5cd6b92fb32fe1adbe44be79a59e86b271d5cb96
Chí Nguyện <83570634+nguyenncai@users.noreply.github.com>
2022-12-15 21:05:55 +0700

    Add files via upload

---
+++ Preprocessing_images_data/convert_am_ban.py
@@ -0,0 +1,12 @@
+import cv2
+import glob
+import os
+dem=0
+for f in glob.glob(r"D:\Breast_Cancer_Detection\train_val\train\*.png"):
+    image=cv2.imread(f)
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #image=cv2.resize(image,(300,300))
+    am_ban = 255 - image
+    cv2.imwrite(f,am_ban)
+    dem+=1
+    print(dem)
